aaaa918744cb4d571d599c5ab4c33210a44f40b3
andrebuenafe <andre.buenafe1437@gmail.com>
2023-07-28 08:45:03 +0800

    initial commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Call the AdminSeeder
+        $this->call(AdminSeeder::class);
+
+        //Call the RolesSeeder
+        $this->call(RolesSeeder::class);
+    }
+}
